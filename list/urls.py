030d91538c5c5c9c21fa50d44528393537f8872a
tiny657 <tiny657@gmail.com>
2019-07-12 01:04:40 -0700

    init

--- list/urls.py
+++ list/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:quiz_id>/', views.answer, name='answer'),
+    path('answer', views.answer, name='answer'),
 ]

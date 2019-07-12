a1f2371f4d58c6cff7f25baa17ab10cf29fef94c
tiny657 <tiny657@gmail.com>
2019-07-12 03:24:19 -0700

    init

--- list/models.py
+++ list/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Quiz(models.Model):
     question = models.TextField()
+    example = models.TextField(default=None, blank=True, null=True)
     question_date = models.DateTimeField()
+    hint = models.TextField(default=None, blank=True, null=True)
     answer = models.TextField(default=None, blank=True, null=True)
     answer_date = models.DateTimeField(default=None, blank=True, null=True)
     right = models.IntegerField(default=0)

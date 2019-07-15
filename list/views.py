3fdcdf652e335038ef71a2899f9e11e1497df05f
tiny657 <tiny657@gmail.com>
2019-07-14 19:00:44 -0700

    init

--- list/views.py
+++ list/views.py
@@ -7,14 +7,30 @@ from .models import Quiz
 
 def list(request):
     quizs = Quiz.objects.order_by('id')
-    context = {'quizs': quizs, 'filter': 1000000}
+    right_quizs = quizs.filter(right=1).count()
+    wrong_quizs = quizs.filter(right=-1).count()
+    context = {
+        'quizs': quizs,
+        'total_quiz': quizs.count(),
+        'right': right_quizs,
+        'wrong': wrong_quizs,
+        'right_percent': right_quizs / quizs.count() * 100,
+        'wrong_percent': wrong_quizs / quizs.count() * 100,
+    }
     return render(request, 'list/list.html', context)
 
 
 def show(request, quiz_id):
     quiz = Quiz.objects.filter(id=quiz_id)
+    next = Quiz.objects.filter(visible=True).filter(id__gt=quiz_id).first()
+
+    # 1: right, -1: wrong, 0: default
     right = request.GET.get('right')
-    context = {'quiz': quiz[0], 'right': right}
+    context = {
+        'quiz': quiz[0],
+        'right': right,
+        'next': next,
+    }
     return render(request, 'list/show.html', context)
 
 

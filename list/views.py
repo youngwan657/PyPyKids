dd2a9351b1080b0d0f3be9d319f2ad2bd872217e
tiny657 <tiny657@gmail.com>
2019-07-16 01:23:58 -0700

    check answer

--- list/views.py
+++ list/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 import datetime
 import subprocess
+import os
 
 from .models import Quiz
 
 
 def list(request):
-    quizs = Quiz.objects.order_by('id')
+    quizs = Quiz.objects.order_by('id').filter(visible=1)
     right_quizs = quizs.filter(right=1).count()
     wrong_quizs = quizs.filter(right=-1).count()
     context = {
@@ -27,9 +28,11 @@ def show(request, quiz_id):
 
     # 1: right, -1: wrong, 0: default
     right = request.GET.get('right')
-    wrong_testcase, actual_answer, expected_answer = "", "", ""
+    wrong_testcase, expected_answer, actual_answer = "", "", ""
     if quiz[0].wrong_testcase != "":
-        wrong_testcase, actual_answer, expected_answer = quiz[0].wrong_testcase.split("\n")
+        wrong_testcase = quiz[0].wrong_testcase.split("\n")[0]
+        actual_answer = quiz[0].wrong_testcase.split("\n")[1]
+        expected_answer = quiz[0].wrong_testcase.split("\n")[2:]
 
     context = {
         'quiz': quiz[0],
@@ -46,7 +49,8 @@ def answer(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     quiz.answer = request.POST['answer']
     quiz.answer_date = datetime.datetime.now()
-    if quiz.testcase == "":
+    print(quiz.testcase)
+    if quiz.testcase == None:
         # subjective quiz
         if quiz.answer.replace(" ", "").strip() == quiz.correct_answer:
             quiz.right = 1
@@ -65,25 +69,47 @@ def checkAnswer(quiz):
     testcase = quiz.testcase.split('\n')[0]
     expected_answer = quiz.testcase.split('\n')[1]
 
-    header = """import sys
+    header = """import sys, ast, os
 """
 
     footer = """
 def main(argv):
     s = Solution()
-    return s.solve(int(argv[0]), int(argv[1]))
-    
+
+    if len(argv) == 1:
+        return s.solve(ast.literal_eval(argv[0]))
+    elif len(argv) == 2:
+        return s.solve(ast.literal_eval(argv[0]), ast.literal_eval(argv[1]))
+    elif len(argv) == 3:
+        return s.solve(ast.literal_eval(argv[0]), ast.literal_eval(argv[1]), ast.literal_eval(argv[2]))
+
 if __name__ == "__main__":
-   sys.exit(main(sys.argv[1:]))
+    answer = main(sys.argv[1:])
+    if os.path.exists("checking_answer"):
+        os.remove("checking_answer")
+    f = open("checking_answer", "w+")
+    f.write("%s" % answer)
+    f.close()
 """
+
     code = header + quiz.answer + footer
-    f = open("/tmp/python3.py", "w+")
+    f = open("checking.py", "w+")
     f.write(code)
     f.close()
-    actual_answer = subprocess.call("python /tmp/python3.py " + testcase, shell=True)
+
+    actual_answer = "None"
+    try:
+        subprocess.run(['python', 'checking.py'] + testcase.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+        if os.path.exists("checking_answer"):
+            f = open("checking_answer", "r")
+            actual_answer = f.read()
+            f.close()
+    except subprocess.CalledProcessError as suberror:
+        actual_answer = suberror.stdout.decode('utf-8')
+
     if str(actual_answer) == expected_answer:
         quiz.right = 1
         quiz.wrong_testcase = ""
     else:
         quiz.right = -1
-        quiz.wrong_testcase = testcase + "\n" + str(actual_answer) + "\n" + expected_answer
+        quiz.wrong_testcase = testcase + "\n" + expected_answer + "\n" + str(actual_answer)

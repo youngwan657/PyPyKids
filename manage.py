18273a2a93a0ec7636f321bec7624271c39a5e64
tiny657 <tiny657@gmail.com>
2019-07-30 01:31:20 -0700

    change project name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python4kid.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ThePythonKids.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

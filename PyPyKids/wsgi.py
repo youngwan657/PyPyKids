9c59f00aa6aeb669ad1abb40f3d5c7b3ea55e168
tiny657 <tiny657@gmail.com>
2019-08-16 14:01:39 -0700

    rename project

--- PyPyKids/wsgi.py
+++ PyPyKids/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ThePythonKids project.
+WSGI config for PyPyKids project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ThePythonKids.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyPyKids.settings')
 
 application = get_wsgi_application()

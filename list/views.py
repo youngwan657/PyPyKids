4556d971b7c0071ce11857e72b5aafd62b49b68f
tiny657 <tiny657@gmail.com>
2019-09-01 02:18:06 -0700

    login error msg

--- list/views.py
+++ list/views.py
@@ -268,6 +268,7 @@ def signout(request):
 
 
 def signin(request):
+    context = {}
     if request.method == 'POST':
         form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
@@ -278,13 +279,12 @@ def signin(request):
                 login(request, user)
                 return redirect('/')
             else:
-                print("invalid")
+                context['login_error'] = 1
         else:
-            print("invalid")
+            context['login_error'] = 1
+
     form = AuthenticationForm()
-    context = {
-        'form': form,
-    }
+    context['form'] = form
     return render(request, "list/signin.html", context)
 
 

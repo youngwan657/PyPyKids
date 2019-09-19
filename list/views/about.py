3beb08c32e6fad5893e7ba7e81f44389ecb023ae
tiny657 <tiny657@gmail.com>
2019-09-19 03:15:50 -0700

    video

--- list/views/about.py
+++ list/views/about.py
@@ -5,11 +5,17 @@ from list.views.common import *
 def about(request):
     user = CustomUser.objects.count()
     quiz = Quiz.objects.count()
+    video = 0
+    for quiz in Quiz.objects.all():
+        if quiz.video != None:
+            video += 1
+
     badge = Badge.objects.count()
 
     context = {
         'username': get_username(request),
         'user': user,
+        'video': video,
         'quiz': quiz,
         'badge': badge,
     }

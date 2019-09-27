cf775ccb313428fffb1fe8d006d9bd0827fb42c2
tiny657 <tiny657@gmail.com>
2019-09-27 09:45:53 -0700

    update db

--- PyPyKids/settings.py
+++ PyPyKids/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'PyPyKids.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'pypykids-254102:us-west2:pypykids',
+        'HOST': 'pypykids-254208:us-west2:pypykids',
         'NAME': 'pypykids',
         'USER': 'root',
         'PASSWORD': '',
